e3bb1a25b94f6f13496e0ed4216eedb6aff9da4f
Alaettin <aldimverdim48@gmail.com>
2020-08-08 19:57:55 +0200

    My first commit from Feature branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,6 +4,8 @@ public class FirstClass {
 public static void main(String[] args) {
 	System.out.println("Our first commit from local repo");
 	System.out.println("Our second commit from local repo");
+	System.out.println("Now I am on my karadayi branch");
+	
 	
 }
 }
